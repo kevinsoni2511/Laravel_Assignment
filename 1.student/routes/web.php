28dd7f93c534924fa2926902f7c9d0e072b84054
KEVIN SONI <168913187+kevinsoni2511@users.noreply.github.com>
2025-02-11 18:44:28 +0530

    Add files via upload

---
+++ 1.student/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\StudentController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route:: post('/submit-form',[StudentController::class,'submitForm']);
+Route::get('/Form',[StudentController::class,'Form']);
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/Form', function () {
+    return view('Form');
+});
